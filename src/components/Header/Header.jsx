9580be154c2c53e913380a4d04432d506e330941
mathieu.nanaki <mathieu.nanaki@gmail.com>
2022-02-18 21:30:16 +0100

    Navbar finished, responsive with burger menu and routes to every page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,114 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { MdOutlineRestaurantMenu } from 'react-icons/md';
+import Logo from '../../assets/images/Logo.png';
+import './Header.css';
 
 function Header() {
+  const [toggleMenu, setToggleMenu] = useState(false);
   return (
-    <header className="header">
-      <h1>Hey ! Header here.</h1>
-    </header>
+    <div>
+      <nav className="app__navbar">
+        <div className="app__navbar-logo">
+          <Link to="/" className="app__Links">
+            <img src={Logo} alt="logo marie cotta" />
+          </Link>
+        </div>
+        <ul className="app__navbar-links">
+          <li className="p__opensans">
+            <Link to="/" className="app__Links">
+              Accueil
+            </Link>
+          </li>
+          <li className="p__opensans">
+            <Link to="/concept" className="app__Links">
+              Concept
+            </Link>
+          </li>
+          <li className="p__opensans">
+            <Link to="/menu" className="app__Links">
+              Menu
+            </Link>
+          </li>
+          <li className="p__opensans">
+            <Link to="/event" className="app__Links">
+              Evénements
+            </Link>
+          </li>
+          <li className="p__opensans">
+            <Link to="/contact" className="app__Links">
+              Contact
+            </Link>
+          </li>
+        </ul>
+        <div className="app__navbar-smallscreen">
+          <GiHamburgerMenu
+            color="#fff"
+            fontSize={27}
+            onClick={() => setToggleMenu(true)}
+          />
+          {toggleMenu && (
+            <div className="app__navbar-smallscreen_overlay flex__center slide-bot">
+              <MdOutlineRestaurantMenu
+                fontSize={27}
+                color="fff"
+                className="overlay__close"
+                onClick={() => setToggleMenu(false)}
+              />
+              <ul className="app__navbar-smallscreen-links">
+                <li className="p__opensans">
+                  <Link
+                    to="/"
+                    className="app__Links"
+                    onClick={() => setToggleMenu(false)}
+                  >
+                    Accueil
+                  </Link>
+                </li>
+                <li className="p__opensans">
+                  <Link
+                    to="/concept"
+                    className="app__Links"
+                    onClick={() => setToggleMenu(false)}
+                  >
+                    Concept
+                  </Link>
+                </li>
+                <li className="p__opensans">
+                  <Link
+                    to="/menu"
+                    className="app__Links"
+                    onClick={() => setToggleMenu(false)}
+                  >
+                    Menu
+                  </Link>
+                </li>
+                <li className="p__opensans">
+                  <Link
+                    to="/event"
+                    className="app__Links"
+                    onClick={() => setToggleMenu(false)}
+                  >
+                    Evénements
+                  </Link>
+                </li>
+                <li className="p__opensans">
+                  <Link
+                    to="/contact"
+                    className="app__Links"
+                    onClick={() => setToggleMenu(false)}
+                  >
+                    Contact
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          )}
+        </div>
+      </nav>
+    </div>
   );
 }
 
